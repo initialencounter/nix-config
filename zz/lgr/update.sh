98459a1fa6ba3901f007dc1f19fc5a48f3e1ab7e
initialencounter <2911583893@qq.com>
2024-10-12 18:10:44 +0800

    sync

--- zz/lgr/update.sh
+++
@@ -1,4 +0,0 @@
-curl -L -o lgr.zip https://mirror.ghproxy.com/https://github.com/LagrangeDev/Lagrange.Core/releases/download/nightly/Lagrange.OneBot_linux-x64_8.0.zip
-unzip lgr.zip
-mv publish/Lagrange.OneBot .
-chmod +x Lagrange.OneBot
