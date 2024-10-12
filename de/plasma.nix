9143ee02db3826605c5dbe12d37c676820f100f0
initialencounter <2911583893@qq.com>
2024-10-13 02:44:35 +0800

    disable plasma

--- de/plasma.nix
+++ de/plasma.nix
@@ -1,7 +1,8 @@
 {lib, ...}:
 {
-  programs.ssh.askPassword = lib.mkForce "/nix/store/65x690xfpm5va91gnh74zkqf7fir4y4b-ksshaskpass-6.1.5/bin/ksshaskpass";
-  services.desktopManager.plasma6.enable = true;
+  # swith to plasma
+  # programs.ssh.askPassword = lib.mkForce "/nix/store/65x690xfpm5va91gnh74zkqf7fir4y4b-ksshaskpass-6.1.5/bin/ksshaskpass";
+  # services.desktopManager.plasma6.enable = true;
   programs.dconf.enable = true;
   programs.kdeconnect.enable = true;
 }
