df1cd71acfd100d1c812f2a03fe2cb39cd0f8065
initialencounter <2911583893@qq.com>
2024-10-13 00:34:35 +0800

    chore

--- programs/fcitx5.nix
+++ programs/fcitx5.nix
@@ -1,7 +1,8 @@
 { config, pkgs, ... }:
 {
    i18n.inputMethod = {
-     enabled = "fcitx5";
+     enable = true;
+     type = "fcitx5";
      fcitx5.addons = with pkgs; [
        fcitx5-gtk             # alternatively, kdePackages.fcitx5-qt
        fcitx5-chinese-addons  # table input method support
