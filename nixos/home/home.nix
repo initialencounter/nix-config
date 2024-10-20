a984542e473f3554f61d1dc67bc3e23b5cda8c07
initialencounter <2911583893@qq.com>
2024-10-20 23:47:51 +0800

    refa. koishi

--- nixos/home/home.nix
+++ nixos/home/home.nix
@@ -6,18 +6,14 @@
   home.homeDirectory = "/home/ie";
   nixpkgs.config.allowUnfree = true;
 
-  imports = [
-    ./koishi.nix
-  ];
 
   # 直接将当前文件夹的配置文件，链接到 Home 目录下的指定位置
   # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
 
   # 递归将某个文件夹中的文件，链接到 Home 目录下的指定位置
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
+  # home.file."bot/ks" = {
+  #   source = ./koishi;
   #   recursive = true;   # 递归整个文件夹
-  #   executable = true;  # 将其中所有文件添加「执行」权限
   # };
 
   # 直接以 text 的方式，在 nix 配置文件中硬编码文件内容
