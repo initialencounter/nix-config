50c34e2c25d92a74a080e92f4739f9c3263b0063
initialencounter <2911583893@qq.com>
2024-11-21 22:07:39 +0800

    drop arion

--- programs/arion.nix
+++ programs/arion.nix
@@ -1,19 +1,8 @@
 { pkgs, ... }: {
-  environment.systemPackages = [
-    pkgs.arion
-
-     # Do install the docker CLI to talk to podman.
-     # Not needed when virtualisation.docker.enable = true;
-    pkgs.docker-client
+  virtualisation.docker.enable = true;
+  environment.systemPackages = with pkgs; [
+    docker
+    docker-compose
+    docker-buildx
   ];
-
-  # Arion works with Docker, but for NixOS-based containers, you need Podman
-  # since NixOS 21.05.
-  virtualisation.docker.enable = false;
-  virtualisation.podman.enable = true;
-  virtualisation.podman.dockerSocket.enable = true;
-  virtualisation.podman.defaultNetwork.settings.dns_enable = true;
-
-  # Use your username instead of `myuser`
-  users.extraUsers.ie.extraGroups = ["podman"];
 }
