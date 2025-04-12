75d3a0ed14f53df5426bcdda6a044fc03a2470a6
initialencounter <2911583893@qq.com>
2025-04-12 23:14:32 +0800

    refactor: napcat service

--- programs/napcat.nix
+++ programs/napcat.nix
@@ -1,5 +1,9 @@
 { config, pkgs, ... }:
 
+let 
+  myNapCat = config._module.args.inputs.napcat.packages.x86_64-linux.default;
+in
+
 {
   systemd.services.napcat = {
     enable = true;
@@ -8,7 +12,7 @@
     wantedBy = [ "multi-user.target" ];
 
     serviceConfig = {
-      ExecStart = "${pkgs.nix}/bin/nix run github:initialencounter/napcat.nix";
+      ExecStart = "${myNapCat}/bin/napcat";
       Restart = "always";
       User = "root";  # 替换为你的用户名
     };
