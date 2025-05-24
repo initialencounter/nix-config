5b25e0196778c63922468cff884d700346ee2446
initialencounter <2911583893@qq.com>
2025-05-25 01:20:32 +0800

    chore

--- nixos/xrdp.nix
+++ nixos/xrdp.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}:{
   # https://discourse.nixos.org/t/configuring-remote-desktop-access-with-gnome-remote-desktop/48023/4
   services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "${pkgs.gnome.gnome-session}/bin/gnome-session";
+  services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
   services.xrdp.openFirewall = true;
 
   # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
