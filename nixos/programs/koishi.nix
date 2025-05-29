502245573813700070eb7c9c912d041a619cf2c4
initialencounter <2911583893@qq.com>
2025-05-30 00:08:36 +0800

    add zhenxun service

--- nixos/programs/koishi.nix
+++ nixos/programs/koishi.nix
@@ -1,12 +1,31 @@
 { pkgs, ... }:
 {
   systemd.services.koishi = {
+    enable = true;
+    description = "Koishi Service";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       ExecStart = "${pkgs.yarn}/bin/yarn start";
       RestartSec = 5;
       Restart = "always";
-      User = "ie";
-      WorkingDirectory = "/home/ie/bot/ks";
+      User = "root";
+      WorkingDirectory = "/root/bot/ie/ks";
+      Environment = "NODE_ENV=production";
+    };
+  };
+
+  systemd.services.zhenxun = {
+    enable = true;
+    description = "Koishi-zhenxun Service";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.yarn}/bin/yarn start";
+      RestartSec = 5;
+      Restart = "always";
+      User = "root";
+      WorkingDirectory = "/root/bot/zhenxun/koishi";
       Environment = "NODE_ENV=production";
     };
   };
