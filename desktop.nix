805eed4beaf07bb0e514ee6be05cc063fe53f326
initialencounter <2911583893@qq.com>
2025-05-30 01:11:17 +0800

    add support for Wayland and install the relevant Wayland tools

--- desktop.nix
+++ desktop.nix
@@ -1,10 +1,18 @@
-{
+{pkgs, ...}:{
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    wofi
+    xdg-desktop-portal-hyprland
+    wayland-protocols
+    wayland-utils
+  ];
 
   # Configure keymap in X11
   services.xserver.xkb = {
