19d8788f8ad57b74e27fd858d1bd46873025ae12
initialencounter <2911583893@qq.com>
2025-05-30 00:07:29 +0800

    fix input method

--- i18n.nix
+++ i18n.nix
@@ -1,25 +1,32 @@
 { pkgs, ... }:
 {
-  # 语言与输入法
-  i18n = {
-    supportedLocales = [
-      "en_US.UTF-8/UTF-8"
-      "zh_CN.UTF-8/UTF-8"
+  # Select internationalisation properties.
+  i18n.defaultLocale = "zh_CN.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "zh_CN.UTF-8";
+    LC_IDENTIFICATION = "zh_CN.UTF-8";
+    LC_MEASUREMENT = "zh_CN.UTF-8";
+    LC_MONETARY = "zh_CN.UTF-8";
+    LC_NAME = "zh_CN.UTF-8";
+    LC_NUMERIC = "zh_CN.UTF-8";
+    LC_PAPER = "zh_CN.UTF-8";
+    LC_TELEPHONE = "zh_CN.UTF-8";
+    LC_TIME = "zh_CN.UTF-8";
+  };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      # for flypy chinese input method
+      fcitx5-rime
+      # needed enable rime using configtool after installed
+      fcitx5-configtool
+      fcitx5-chinese-addons
+      # fcitx5-mozc    # japanese input method
+      fcitx5-gtk # gtk im module
     ];
-    defaultLocale = "zh_CN.UTF-8";
-    extraLocaleSettings = {
-      LANGUAGE = "zh_CN.UTF-8";
-      LC_ALL = "zh_CN.UTF-8";
-    };
-    inputMethod = {
-      enable = true;
-      type = "fcitx5";
-      fcitx5.addons = with pkgs; [
-        fcitx5-mozc
-        fcitx5-gtk
-        fcitx5-chinese-addons
-      ];
-    };
   };
 
   console = {
