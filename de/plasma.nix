0be9d1bb6b53ad69c28c426469b7aa4b2014fe63
initialencounter <2911583893@qq.com>
2025-05-29 23:07:43 +0800

    only wayland

--- de/plasma.nix
+++
@@ -1,9 +0,0 @@
-{lib, ...}:
-{
-  services.desktopManager.plasma6.enable = true;
-  services.displayManager = {
-    defaultSession = "plasmax11";  # Plasma (X11)
-  };
-  programs.dconf.enable = true;
-  programs.kdeconnect.enable = true;
-}
