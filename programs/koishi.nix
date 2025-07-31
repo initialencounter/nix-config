68d740999f6ac8996a7e7b062eb54775f4767a08
initialencounter <2911583893@qq.com>
2025-07-31 23:11:48 +0800

    chore

--- programs/koishi.nix
+++ programs/koishi.nix
@@ -1,19 +1,22 @@
 { pkgs, ... }:
 {
-  systemd.services.koishi = {
-    enable = true;
-    description = "Koishi Service";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.yarn}/bin/yarn start";
-      RestartSec = 5;
-      Restart = "always";
-      User = "root";
-      WorkingDirectory = "/root/bot/ie/ks";
-      Environment = "NODE_ENV=production";
-    };
-  };
+  # systemd.services.koishi = {
+  #   enable = true;
+  #   description = "Koishi Service";
+  #   after = [ "network.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.yarn}/bin/yarn start";
+  #     RestartSec = 5;
+  #     Restart = "always";
+  #     User = "root";
+  #     WorkingDirectory = "/root/bot/ie/ks";
+  #     Environment = [
+  #       "NODE_ENV=production"
+  #       "LD_LIBRARY_PATH=${pkgs.fontconfig.lib}/lib:${pkgs.freetype}/lib"
+  #     ];
+  #   };
+  # };
 
   systemd.services.zhenxun = {
     enable = true;
